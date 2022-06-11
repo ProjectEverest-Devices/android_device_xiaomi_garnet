2cfbcebe12550043d04d8b261a7cbcfb6ad35fa9
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-06-11 02:02:20 +0000

    garnet: Enable freeform windows
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- device.mk
+++ device.mk
@@ -237,6 +237,10 @@ PRODUCT_COPY_FILES += \
 # Firmware
 $(call inherit-product-if-exists, vendor/xiaomi/firmware/garnet/config.mk)
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
